1b183a311ce6c67d96a80b7843311df15845cad9
KJSDR <killian.delamarre-ruer@students.dominican.edu>
2024-11-12 09:15:24 -0800

    With challenge

--- game.py
+++ game.py
@@ -1,22 +1,38 @@
-import pygame 
+# Import and initialize pygame
+import pygame
 pygame.init()
-
 # Configure the screen
 screen = pygame.display.set_mode([500, 500])
 
+circle_color = (50, 50, 50)
 # Creat the game loop
-running = True 
-while running: 
-	# Looks at events 
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			running = False
-	
-	# Clear the screen
-	screen.fill((255, 255, 255))
-	# Draw a circle
-	color = (0, 255, 255)
-	position = (250, 250)
-	pygame.draw.circle(screen, color, position, 75)
-	# Update the display
-	pygame.display.flip()
+running = True
+while running:
+    # Looks at events 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    # Clear the screen
+    screen.fill((255, 255, 255))
+    # Draw a circle
+    circle_positions = [
+        (100, 100),
+        (400, 100),
+        (250, 250),
+        (100, 400),
+        (400, 400)
+    ]
+    circle_colors = [
+        (255, 25, 0),   
+        (255, 153, 0),  
+        (255, 247, 0),  
+        (0, 255, 4),
+        (17, 0, 255) 
+    ]
+
+    for i, position in enumerate(circle_positions):
+        pygame.draw.circle(screen, circle_colors[i], position, 30)
+    # Update the display
+    pygame.display.flip()
+
+
