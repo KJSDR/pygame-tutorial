6e7631284f003384a5afde81c2212bf524bc7575
KJSDR <killian.delamarre-ruer@students.dominican.edu>
2024-11-12 09:39:08 -0800

    Add files via upload

--- game.py
+++ game.py
@@ -1,38 +1,44 @@
-# Import and initialize pygame
 import pygame
+
+# Initialize Pygame
 pygame.init()
-# Configure the screen
+
+# Set up the screen
 screen = pygame.display.set_mode([500, 500])
 
-circle_color = (50, 50, 50)
-# Creat the game loop
+# Game Object
+class GameObject(pygame.sprite.Sprite):
+  # Remove width and height and add image here!
+  def __init__(self, x, y, image):
+    super(GameObject, self).__init__()
+    # self.surf = pygame.Surface((width, height)) REMOVE!
+    # self.surf.fill((255, 0, 255)) REMOVE!
+    self.surf = pygame.image.load(image) # ADD!
+    self.x = x
+    self.y = y
+
+  def render(self, screen):
+    screen.blit(self.surf, (self.x, self.y))  
+
+# Make an instance of GameObject
+# box = GameObject(120, 300, 50, 50) REMOVE!
+apple = GameObject(120, 300, 'apple.png') # ADD!
+
+# Game loop
 running = True
 while running:
-    # Looks at events 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
-    # Clear the screen
-    screen.fill((255, 255, 255))
-    # Draw a circle
-    circle_positions = [
-        (100, 100),
-        (400, 100),
-        (250, 250),
-        (100, 400),
-        (400, 400)
-    ]
-    circle_colors = [
-        (255, 25, 0),   
-        (255, 153, 0),  
-        (255, 247, 0),  
-        (0, 255, 4),
-        (17, 0, 255) 
-    ]
+            running = False  
 
-    for i, position in enumerate(circle_positions):
-        pygame.draw.circle(screen, circle_colors[i], position, 30)
+    # Fill the screen with a white background
+    screen.fill((255, 255, 255))
+
+    # box.render(screen) REMOVE!
+    apple.render(screen) # ADD!
+
     # Update the display
     pygame.display.flip()
 
-
+# Quit Pygame
+pygame.quit()
