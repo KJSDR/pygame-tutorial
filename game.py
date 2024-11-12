9a1228bada82e4862c72feeab2be9feb3f5fd787
KJSDR <killian.delamarre-ruer@students.dominican.edu>
2024-11-12 10:12:55 -0800

    Add files via upload

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import pygame
+from random import randint
 
 # Initialize Pygame
 pygame.init()
@@ -6,23 +7,42 @@ pygame.init()
 # Set up the screen
 screen = pygame.display.set_mode([500, 500])
 
-# Game Object
+# Get the clock
+clock = pygame.time.Clock()
+
+# Create the GameObject class
 class GameObject(pygame.sprite.Sprite):
-  # Remove width and height and add image here!
-  def __init__(self, x, y, image):
-    super(GameObject, self).__init__()
-    # self.surf = pygame.Surface((width, height)) REMOVE!
-    # self.surf.fill((255, 0, 255)) REMOVE!
-    self.surf = pygame.image.load(image) # ADD!
-    self.x = x
-    self.y = y
+    def __init__(self, x, y, image):
+        super(GameObject, self).__init__()
+        self.surf = pygame.image.load(image)
+        self.rect = self.surf.get_rect()
+        self.rect.topleft = (x, y)
 
-  def render(self, screen):
-    screen.blit(self.surf, (self.x, self.y))  
+    def render(self, screen):
+        screen.blit(self.surf, self.rect)
 
-# Make an instance of GameObject
-# box = GameObject(120, 300, 50, 50) REMOVE!
-apple = GameObject(120, 300, 'apple.png') # ADD!
+# Create an Apple class
+class Apple(GameObject):
+    def __init__(self):
+        super(Apple, self).__init__(0, 0, 'apple.png')
+        self.dx = 0
+        self.dy = (randint(0, 200) / 100) + 1
+        self.reset()  # call reset here!
+
+    def move(self):
+        self.rect.x += self.dx
+        self.rect.y += self.dy
+        # Check the y position of the apple
+        if self.rect.y > 500:
+            self.reset()
+
+    # add a new method
+    def reset(self):
+        self.rect.x = randint(50, 400)
+        self.rect.y = -64
+
+# Create an instance of Apple
+apple = Apple()
 
 # Game loop
 running = True
@@ -34,11 +54,12 @@ while running:
     # Fill the screen with a white background
     screen.fill((255, 255, 255))
 
-    # box.render(screen) REMOVE!
-    apple.render(screen) # ADD!
+    # Draw apple
+    apple.move()
+    apple.render(screen)
 
-    # Update the display
+    # Update the window
     pygame.display.flip()
 
-# Quit Pygame
-pygame.quit()
+    # Tick the clock!
+    clock.tick(60)
